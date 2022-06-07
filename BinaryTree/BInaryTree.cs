cabe6a3c3a7585ad27dbe516140e69a22f57be67
castaval <alexander.kiosk@mail.ru>
2022-06-07 20:48:57 +0300

    feat: add remove 1/3

--- BinaryTree/BInaryTree.cs
+++ BinaryTree/BInaryTree.cs
@@ -7,10 +7,15 @@ public class BinaryTree<T> where T: IPlayer
 
     public int Index { get; set; } = 0;
 
-    public void Insert (T data)
+    public void Insert(T data)
     {
         Root = InnerInsert(data, Root);
         Root.index = Index++;
+    }
+
+    public void Remove(T data)
+    {
+        Root = RemoveInner(Root, data);
     }
 
     private Node<T> InnerInsert(T data, Node<T>? root)
@@ -28,5 +33,42 @@ public class BinaryTree<T> where T: IPlayer
         }
 
         return root;
+    }
+
+    private Node<T>? RemoveInner(Node<T>? parent, T data)
+    {
+        if (parent == null) return parent;
+
+        if (data.Strength < parent.Data.Strength)
+            parent.NodeLeft = RemoveInner(parent.NodeLeft, data);
+        else if (data.Strength > parent.Data.Strength)
+            parent.NodeRight = RemoveInner(parent.NodeRight, data);
+
+        else
+        {
+            if (parent.NodeLeft == null)
+                return parent.NodeRight;
+            else if (parent.NodeRight == null)
+                return parent.NodeLeft;
+
+            parent.Data.Strength = MinValue(parent.NodeRight);
+
+            parent.NodeRight = RemoveInner(parent.NodeRight, parent.Data);
+        }
+
+        return parent;
+    }
+
+    private int MinValue(Node<T> node)
+    {
+        int minValue = node.Data.Strength;
+
+        while (node.NodeLeft != null)
+        {
+            minValue = node.NodeLeft.Data.Strength;
+            node = node.NodeLeft;
+        }
+
+        return minValue;
     }
 }

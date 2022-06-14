33e41471e7d944a8cb0be493277347064698991c
castaval <alexander.kiosk@mail.ru>
2022-06-14 17:45:46 +0300

    refactor: binary tree dir

---
+++ RpgSaga/BinaryTree/Node.cs
@@ -0,0 +1,21 @@
+namespace BinaryTreePlayers
+{
+    using Players;
+
+    public class Node<T> where T: IPlayer
+    {
+        public T Data { get; set; }
+
+        public int Index { get; set; } = 0;
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node<T>? NodeLeft { get; set; } = null;
+        public Node<T>? NodeRight { get; set; } = null;
+    }
+
+}
+
